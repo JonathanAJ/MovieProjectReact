1f0a5292f46e79fcaf393f3e1470ef30cb2e0eac
Jonathan Alves <alves_aj@windowslive.com>
2017-05-23 00:40:28 -0300

    melhora bd

--- app/dao/ConversaDAO.js
+++ app/dao/ConversaDAO.js
@@ -6,10 +6,7 @@ import { UsuarioDAO } from './UsuarioDAO';
 export class ConversaDAO{
 	
 	constructor(context){
-		this.database = firebase.database();
-		this.refChats = this.database.ref('chats');
-		this.refUsers = this.database.ref('users');
-		this.refMsgs = this.database.ref('messages');
+		this.db = firebase.database();
 		this.context = context;
 		this.arrayMessageExist = [];
 		this.usuarioDAO = new UsuarioDAO(context);
@@ -26,18 +23,19 @@ export class ConversaDAO{
 	initConversas(callback){
 		const me = this.me;
 
-		this.refUsers.child(me.uid + '/my_chats').once('value', snap => {
+		this.db.ref('users/' + me.uid + '/my_chats').once('value', snap => {
 
 			snap.forEach(childSnapshot => {
 				
 				const key = childSnapshot.key;
 				const childData = childSnapshot.val();
 
-				this.refMsgs.child(key).once('value', snap => {
+
+				this.db.ref('messages/' + key).once('value', snap => {
 					
 					if(snap.exists()){
 
-						this.refChats.child(key).once('value', snap => {
+						this.db.ref('chats/' + key).on('value', snap => {
 							console.log('ok')
 
 							let obj = snap.val();
